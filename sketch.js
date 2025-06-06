616dad1d96e773a430ffe34760d8c6ef647325ca
Jun Ozaki <79558760+O685463@users.noreply.github.com>
2025-06-07 03:28:59 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
+
 function draw() {
-  background(255);
+  background(255); // 白い背景
   for (var y = 0; y <= 1000; y = y + 500) {
     for (var x = 0; x <= 1000; x = x + 500) {
-      noFill();
-      stroke(255, 147, 206);
+      noFill(); // 塗りつぶしなし
+      stroke(255, 147, 206); // ピンク色の線
       rotateX(frameCount * 0.01);
       rotateY(frameCount * 0.01);
       box(200, 200, 200);
